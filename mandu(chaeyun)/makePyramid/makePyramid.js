269e9ca4374566ec4604e4be2fa1897ced2a9155
Chaeyun <pcpclee1@naver.com>
2017-01-08 16:21:56 +0900

    Update pyramid to diamond.

--- mandu(chaeyun)/makePyramid/makePyramid.js
+++ mandu(chaeyun)/makePyramid/makePyramid.js
@@ -2,13 +2,13 @@ function makePyramid(lineNumber) {
   var arr = [];
   var star = "*";
   var space = " ";
-  var indexNumber = lineNumber * 2;
+  var indexNumber = lineNumber * 2 - 1;
 
   for(var i = 0; i < indexNumber; i++){
     arr.push(space);
   }
 
-  var centerStar_index = Math.floor(indexNumber/2 - 1);
+  var centerStar_index = Math.floor(indexNumber/2);
 
   for(var j=0; j<lineNumber; j++){
     arr[centerStar_index - j] = star;
@@ -16,17 +16,21 @@ function makePyramid(lineNumber) {
     var eachLine = arr.join(space);
     console.log(eachLine);
   }
+  for(var k=0; k<lineNumber; k++){
+    arr[k] = space;
+    arr[(indexNumber-1) - k] = space;
+    var eachLine2 = arr.join(space);
+    console.log(eachLine2);
+  }
 }
-//makePyramid(10);
+makePyramid(4);
 /*
-                   *
-                 * * *
-               * * * * *
-             * * * * * * *
-           * * * * * * * * *
-         * * * * * * * * * * *
-       * * * * * * * * * * * * *
-     * * * * * * * * * * * * * * *
-   * * * * * * * * * * * * * * * * *
- * * * * * * * * * * * * * * * * * * *
+makePyramid.js:17       *
+makePyramid.js:17     * * *
+makePyramid.js:17   * * * * *
+makePyramid.js:17 * * * * * * *
+makePyramid.js:23   * * * * *
+makePyramid.js:23     * * *
+makePyramid.js:23       *
+makePyramid.js:23
 */
