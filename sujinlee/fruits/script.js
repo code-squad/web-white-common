f89f6aaed8e8325b52640adbd6bec957664cec96
Sujin Lee <sujinlee.me@gmail.com>
2017-01-12 11:24:35 +0900

    미션 4번 수정

--- sujinlee/fruits/script.js
+++ sujinlee/fruits/script.js
@@ -1,5 +1,4 @@
-// 미션 3. 
-
+// 미션 3-1.strawberry아래에 새로운 과일 pineapple 추가
 function addFruits() {
     var node = document.createElement("li");
     var textnode = document.createTextNode("pineapple");
@@ -7,15 +6,23 @@ function addFruits() {
     document.querySelectorAll('ul')[0].appendChild(node);
 }
 
-// 미션 4.
+// 미션 3-2. 과일 삭제
 function removeFruits() {
-    var lst = document.querySelectorAll('ul')[0], item = lst.lastElementChild;
-    lst.removeChild(item);
+    var node = document.querySelectorAll('ul')[0],
+        item = node.lastElementChild;
+    node.removeChild(item);
 }
 
+// 미션 4.banana와 orange사이에 새로운 과일 추가
+function insertFruits() {
+    var node = document.querySelectorAll('ul')[0],
+        item = node.children[1],
+        li = document.createElement('li')
+        li.innerHTML = "peach";
+    item.parentNode.insertBefore(li, item);
+}
 
-
-// 미션 5. apple 을 grape와 strawberry 사이로 옮김
+// 미션 5. apple을 grape와 strawberry 사이로 옮김
 function changeFruits(){
     var node = document.querySelectorAll("ul")[0],
         x = node.firstElementChild,
