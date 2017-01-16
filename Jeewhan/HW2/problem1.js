943fbbc75c3cb907d06aaa6dae8e12e860cdd539
Jeewhan <distinct19@gmail.com>
2017-01-16 23:29:40 +0900

    Update

--- Jeewhan/HW2/problem1.js
+++ Jeewhan/HW2/problem1.js
@@ -75,20 +75,21 @@ function executeItemNode(actionType, todoORnumber) {
 /*
  * 3번문제는 여기에 자세히 설명을 넣으시면 됩니다.
 
- * querySelector 축약
+ // querySelector 축약
 
- * element의 addEventListener(event, function) method를 호출해, eventHandler 등록
+ // element의 addEventListener(event, function) method를 호출해, eventHandler 등록
 
- * click 이벤트 발생시 target이
- * 버튼이 아닐 때는 return을 통해 Pass
+ // click 이벤트 발생시 target이
+ // 버튼이 아닐 때는 return을 통해 Pass
 
- * button이 맞을 때는
- * button 태그 이전에 있는 Input Tag의 값을 inputValue에 할당
- * button의 클래스명은 actionType에 할당
- * 두 변수를 executeItemNode의 Parameter로 넘겨서 호출
+ // button이 맞을 때는
+ // button 태그 이전에 있는 Input Tag의 값을 inputValue에 할당
+ // button의 클래스명은 actionType에 할당
+ // 두 변수를 executeItemNode의 Parameter로 넘겨서 호출
 
  */
 
+// 아래부터는 모두 문제 조건에 제시된 코드
 var controller = document.querySelector(".controller");
 
 controller.addEventListener("click", function(evt) {
@@ -98,3 +99,32 @@ controller.addEventListener("click", function(evt) {
   var actionType = btn.className;
   executeItemNode(actionType, inputValue);
 });
+
+
+// 73행까지의 코드에 대한 피드백
+
+/* executeItemNode 로 감싸서 전역변수를 없앴지만,
+
+ * executeItemNode 에 너무 많은 로직이 다 들어가 있네요. 그 안에서도 함수를 나눠서 만들고 호출하세요.
+
+* if문 안에 if를 없애보려고 노력하세요. 
+
+* $("section.basket > ol") 이게 반복적으로 사용하는데, 변수에 담아두고 재사용하는게 더 빠릅니다.
+
+* for 문안에  $("section.basket > ol").children.length 이렇게 있으니 좀 보기 어렵네요. 변수로 받아두고 쓰셔도 좋겠고요.
+
+* $("section.basket > ol").children[i].innerHTML = toDoList[i]; 이거 제대로 화면에 들어가나요?
+
+*/
+
+// 하고 싶었지만 하지 못 한 반론
+
+// 1. 한 번 정도 호출될 함수일 경우, 함수명에 프로세스와 리턴벨류를 아주 잘 담아낼 수 없다면,
+//    함수를 선언하는 것이 가독성에 반대로 악영향을 줄 수 있다고 생각합니다. (함수명만으로 확 와닿지 않는다면 번거롭게 함수 선언 부분을 계속 찾아가야 하는 상황이 생길 수 있으므로)
+
+// 2. if의 분기마다 한 번에 하나의 조건별로 분기를 하는 것이 독자 입장에서 더 좋다고 생각합니다.
+//    T & T 또는 T & F 처럼 적게 되면, IF 안 IF를 없앨 수는 있겠지만, 한 번에 고려해야 하는 조건이 많아져서 나쁘다고 생각합니다.
+
+// 3. $("section.basket > ol")은 변수로 담아서 사용하고 싶었지만, Call By Value가 되어버려서 결과가 잘못 나오게 되는 바람에 하지 못 했습니다.
+
+// 4. 화면에 제대로 들어갑니다 :D
