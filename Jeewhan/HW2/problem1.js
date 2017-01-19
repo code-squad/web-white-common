16342446b9d0d27aa35ee83d1c47cea072e48dbf
Jeewhan <distinct19@gmail.com>
2017-01-19 10:08:12 +0900

    Update

--- Jeewhan/HW2/problem1.js
+++ Jeewhan/HW2/problem1.js
@@ -1,98 +1,102 @@
-/*
- * 1. executeItemNode 함수를 완성하세요
- * 이 함수의 actionType 매개변수는 'add' 또는 'remove'를 받습니다.  add를 받으면 추가하고, remove를 받으면 일을 삭제합니다.
- * todoORnumber 는 add일때는 새로운 일을 문자열로 받고, remove일때는 숫자를 받습니다.
- * 할 일 목록은 할일의 문자열 길이 순으로 정렬됩니다. 목록이 추가될때마다 바로 정렬되야 합니다.(가장 긴 할일 내용이 뒤로 가야함)
- * 추가하려는 경우 이미 같은 일이 있다면 message영역에서 적당한 메시지를 붉은색으로 표시했다 3초뒤 사라집니다.
- * 삭제하려는 경우 num과 일치하는 item번호가 없다면 'message' 영역에서 적당한 메시지를 붉은색으로 표시됐다 3초뒤 사라집니다.
- * 함수를 여러개로 나눠도 상관없습니다.
- * 참고로 1번을 풀기 위해서는 string조작과 setTimeout이라는 것을 공부해야 할 수도 있습니다.
- */
+function $(element) {
+  return document.querySelector(element);
+}
 
-function executeItemNode(actionType, todoORnumber) {
+var sectionBasketOl = function(element) {
+  return $("section.basket > ol");
+};
 
-  const ERROR_MSG = {
-    "add" : {
-      "EXIST_TODO" : "이미 등록한 일입니다."
-    },
-    "remove" : {
-      "NON_EXIST_TODO" : "삭제하려는 할 일이 없습니다."
-    },
-  };
+const ERROR_MSG = {
+  "add" : "이미 등록한 일입니다.",
+  "remove" : "삭제하려는 할 일이 없습니다."
+};
 
-  function $(element) {
-    return document.querySelector(element);
+function checkError(type, value) {
+  if (type === "add" && toDoArr.indexOf(value) !== -1) {
+    alertMessage($("div.message"), ERROR_MSG.add); return true;
   }
-
-  function alertMessage(target, comment) {
-    target.insertAdjacentHTML("afterbegin", "<p>" + comment + "</p>");
-    setTimeout(function() { target.removeChild(target.firstElementChild); }, 3000);
+  else if (type === "remove" && value > toDoArr.length) {
+    alertMessage($("div.message"), ERROR_MSG.remove); return true;
   }
+}
 
-  function pushToDo(toDo) {
-    toDoList.push(toDo);
-    toDoList.sort();
+function alertMessage(target, comment) {
+  target.insertAdjacentHTML("afterbegin", "<p>" + comment + "</p>");
+  setTimeout(function() { target.removeChild(target.firstElementChild); }, 3000);
+}
 
-    toDoList.sort(function(firstElement, secondElement) {
-        return firstElement.length - secondElement.length;
-    });
+function addToDo(todo) {
 
-    sectionBasketOl().remove();
-    $("section.basket").appendChild(document.createElement("ol"));
+  let todoNode = "<li>" + todo + '<button type="button" class="xButton">x</button></li>';
 
-    for (let i = 0; i < toDoList.length; i++) {
-      sectionBasketOl().insertAdjacentHTML("beforeend", "<li>" + toDoList[i] + "</li>");
+  for (let i = 0; i < toDoArr.length; i++) {
+    if (todo.length <= toDoArr[i].length) {
+      let insertIndex = i + 1;
+
+      lastChild = false;
+      let nextNode = $("li:nth-child(" + insertIndex + ")");
+
+      nextNode.insertAdjacentHTML("beforebegin", todoNode);
+      toDoArr.splice(insertIndex, 0, todo);
+      return;
     }
   }
 
-  let message = $("div.message");
+  if (toDoArr.length === 0) {
+    $(".toDoList").appendChild(document.createElement("li"));
+    $(".toDoList > li").innerHTML = todo + '<button type="button" class="xButton">x</button>';
+    toDoArr.push(todo);
+    return;
+  }
 
-  // --------------------
+  $(".toDoList").lastElementChild.insertAdjacentHTML("afterend", todoNode);
+}
 
-  if (actionType === "add" && toDoList.indexOf(todoORnumber) === -1) {
-    pushToDo(todoORnumber);
+function removeToDo(index) {
+  toDoArr.splice(index-1, 1);
+  sectionBasketOl().children[index-1].remove();
+}
+
+function xToDo(eventObj) {
+  let node = eventObj.parentNode;
+  let xNodeText = toDoArr.indexOf(node.textContent.slice(0,-1));
+
+  toDoArr.splice(xNodeText, 1);
+  node.remove();
+}
+
+function determineType(type, value) {
+  if (type === "add") {
+    addToDo(value);
   }
-  else if (actionType === "add" && toDoList.indexOf(todoORnumber) !== -1) {
-    alertMessage(message, ERROR_MSG[actionType].EXIST_TODO);
+  else if (type === "remove") {
+    removeToDo(value);
   }
-  else if (actionType === "remove" && toDoList[todoORnumber - 1] === undefined) {
-    alertMessage(message, ERROR_MSG[actionType].NON_EXIST_TODO);
-  }
-  else if (actionType === "remove" && toDoList[todoORnumber - 1] !== undefined) {
-    sectionBasketOl().children[todoORnumber-1].remove();
+  else if (type === "xButton") {
+    xToDo(value);
   }
 }
 
-
-var sectionBasketOl = function(element) {
-  return document.querySelector("section.basket > ol");
-};
-
-var toDoList = [];
+var toDoArr = [];
 
 for (let i = 0; i < sectionBasketOl().children.length; i++) {
-  toDoList.push(sectionBasketOl().children[i].innerHTML);
+  toDoArr.push(sectionBasketOl().children[i].textContent.slice(0,-1));
 }
-toDoList.sort();
 
+$(".controller").addEventListener("click", function(evt) {
+  let btn = evt.target;
+  let actionType = btn.className;
+  let inputValue = btn.previousElementSibling.value;
 
-var controller = document.querySelector(".controller");
+  if (btn.tagName !== "BUTTON" || checkError(actionType, inputValue)) return;
 
-controller.addEventListener("click", function(evt) {
-  var btn = evt.target;
-  if (btn.tagName !== "BUTTON") return;
-  var inputValue = btn.previousElementSibling.value;
-  var actionType = btn.className;
-  executeItemNode(actionType, inputValue);
+  determineType(actionType, inputValue);
 });
 
-// 개선해야 할 점 : 전체를 다시 구현하지 않고, 추가해야 할 지점만 찾아서 넣어주기
+$(".toDoList").addEventListener("click", function(evt) {
+  let btn = evt.target;
+  let actionType = btn.className;
+  if (btn.tagName !== "BUTTON" || btn.className !== "xButton") return;
 
-const actionObj = {
-  "add" : function() {
-    // do something
-  },
-  "remove" : function() {
-    // do something
-  },
-};
+  determineType(actionType, btn);
+});
---
+++ Jeewhan/HW2/problem2.js
@@ -0,0 +1,114 @@
+/*
+ * 1. executeItemNode 함수를 완성하세요
+ * 이 함수의 actionType 매개변수는 'add' 또는 'remove'를 받습니다.  add를 받으면 추가하고, remove를 받으면 일을 삭제합니다.
+ * todoORnumber 는 add일때는 새로운 일을 문자열로 받고, remove일때는 숫자를 받습니다.
+ * 할 일 목록은 할일의 문자열 길이 순으로 정렬됩니다. 목록이 추가될때마다 바로 정렬되야 합니다.(가장 긴 할일 내용이 뒤로 가야함)
+ * 추가하려는 경우 이미 같은 일이 있다면 message영역에서 적당한 메시지를 붉은색으로 표시했다 3초뒤 사라집니다.
+ * 삭제하려는 경우 num과 일치하는 item번호가 없다면 'message' 영역에서 적당한 메시지를 붉은색으로 표시됐다 3초뒤 사라집니다.
+ * 함수를 여러개로 나눠도 상관없습니다.
+ * 참고로 1번을 풀기 위해서는 string조작과 setTimeout이라는 것을 공부해야 할 수도 있습니다.
+ */
+
+function executeItemNode(actionType, todoORnumber) {
+
+  const ERROR_MSG = {
+    "add" : {
+      "EXIST_TODO" : "이미 등록한 일입니다."
+    },
+    "remove" : {
+      "NON_EXIST_TODO" : "삭제하려는 할 일이 없습니다."
+    },
+  };
+
+  function $(element) {
+    return document.querySelector(element);
+  }
+
+  function alertMessage(target, comment) {
+    target.insertAdjacentHTML("afterbegin", "<p>" + comment + "</p>");
+    setTimeout(function() { target.removeChild(target.firstElementChild); }, 3000);
+  }
+
+  function pushToDo(toDo) {
+    toDoList.push(toDo);
+    toDoList.sort();
+
+    toDoList.sort(function(firstElement, secondElement) {
+        return firstElement.length - secondElement.length;
+    });
+
+    sectionBasketOl().remove();
+    $("section.basket").appendChild(document.createElement("ol"));
+
+    for (let i = 0; i < toDoList.length; i++) {
+      sectionBasketOl().insertAdjacentHTML("beforeend", "<li>" + toDoList[i] + "</li>");
+    }
+  }
+
+  let message = $("div.message");
+
+  // --------------------
+
+  if (actionType === "add" && toDoList.indexOf(todoORnumber) === -1) {
+    pushToDo(todoORnumber);
+  }
+  else if (actionType === "add" && toDoList.indexOf(todoORnumber) !== -1) {
+    alertMessage(message, ERROR_MSG[actionType].EXIST_TODO);
+  }
+  else if (actionType === "remove" && toDoList[todoORnumber - 1] === undefined) {
+    alertMessage(message, ERROR_MSG[actionType].NON_EXIST_TODO);
+  }
+  else if (actionType === "remove" && toDoList[todoORnumber - 1] !== undefined) {
+    sectionBasketOl().children[todoORnumber-1].remove();
+  }
+  // else if (actionType === "delete") {
+  //   todoORnumber.parentNode.remove();
+  // }
+}
+
+
+var sectionBasketOl = function(element) {
+  return document.querySelector("section.basket > ol");
+};
+
+var toDoList = [];
+
+for (let i = 0; i < sectionBasketOl().children.length; i++) {
+  toDoList.push(sectionBasketOl().children[i].innerHTML);
+}
+toDoList.sort();
+
+
+var controller = document.querySelector(".controller");
+
+controller.addEventListener("click", function(evt) {
+  var btn = evt.target;
+  if (btn.tagName !== "BUTTON") return;
+  var inputValue = btn.previousElementSibling.value;
+  var actionType = btn.className;
+  executeItemNode(actionType, inputValue);
+});
+
+function toDoRemove(element) {
+  element.parentNode.remove();
+}
+
+// var removeButton = document.querySelector(".toDoList");
+//
+// removeButton.addEventListener("click", function(evt) {
+//   console.log(evt.target);
+//   var btn = evt.target;
+//   if (btn.tagName !== "BUTTON") return;
+//   executeItemNode("delete", btn);
+// });
+
+// 개선해야 할 점 : 전체를 다시 구현하지 않고, 추가해야 할 지점만 찾아서 넣어주기
+
+const actionObj = {
+  "add" : function() {
+    // do something
+  },
+  "remove" : function() {
+    // do something
+  },
+};
