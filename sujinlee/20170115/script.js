e9bd6cfb8f7e2aec61968a50b64f57b6bfce6636
sujinleeme <sujinlee.me@gmail.com>
2017-01-20 18:07:25 +0900

    tabUI css 일부완성

--- sujinlee/20170115/script.js
+++ sujinlee/20170115/script.js
@@ -1,97 +1,115 @@
-// -- 목록 추가 -- //
-function addItem(newItem) {
-    // <li>요소 확인
-    const node = document.getElementsByTagName("ol")[0];
-    const li = document.createElement("li");
-    var h3 = document.getElementsByTagName("h3")[0];
-    var arr = [],
-        elements = node.children;
+// MESSAGES
+var ERROR_MSG = {
+    "TODO": {
+        "EXIST_TODO" : "이미 등록한 일입니다.",
+        "EMPTY" : "현재 할일이 없습니다. 새로운 할일을 추가하세요."
+    },
+    "INPUT": {
+        "EMPTY" : "입력하세요.",
+        "ZERO" : "1부터 입력하세요.",
+        "NOT_NUMBER" : "숫자를 입력하세요.",
+        "NOT_IN_LIST" : "목록에 없는 번호입니다."
+    }
+}
+
+var SUCCESS_MSG = {
+    "TODO" : {
+        "ADD_TODO" : "새로운 할일을 추가했습니다",
+        "REMOVE_TODO" : "할일이 삭제되었습니다."
+    }
+}
+
+
+//RAISE ERROR MESSAGE
+function chkErrInput(num){
     var msg;
-    for (var i=0; i < elements.length; i++){
-        arr[i] = elements[i].textContent;
+    if (num.length == 0) {
+        msg = ERROR_MSG.INPUT.EMPTY;
+    }
+    else if (num.match(/^[0-9]+$/) === null){
+        msg = ERROR_MSG.INPUT.NOT_NUMBER;
+    }
+    else if(taskList.length >= 1 && num >= taskList.length+1) {
+        msg = ERROR_MSG.INPUT.NOT_IN_LIST;
+    }
+    else if (num == 0) {
+        msg = ERROR_MSG.INPUT.ZERO;
+    }
+    return msg;   
+}
+
+
+// -- DOM ELEMENTS -- //
+const taskNode = document.getElementsByTagName("ol")[0];
+var taskList = taskNode.children;
+var countTask = document.getElementsByTagName("h3")[0];
+const li = document.createElement("li");
+
+
+// -- ADD TASK IN LIST -- //
+function addTask(newTask) {
+    var arr = [];
+    var msg;
+    for (var i=0; i < taskList.length; i++){
+        arr[i] = taskList[i].textContent;
     }
 
-    // 목록 내 중복 값 확인
-    if (arr.indexOf(newItem) !== -1 ) {
-        msg = "이미 등록된 할일입니다.";
-        showMessage(msg);
+    // Emtpy List
+    if (arr.indexOf(newTask) !== -1 ) {
+        msg = ERROR_MSG.TODO.EXIST_TODO;
     }
+    // Add task in list
     else{
-        arr.push(newItem);
-        // elements 개수 확인 후, string길이 오름차순 목록 정렬
+        arr.push(newTask);
         arr.sort(function(a, b){ return a.length - b.length; });
-        for (var i=0; i < elements.length; i++){ 
-            elements[i].innerHTML = arr[i];
+        for (var i=0; i < taskList.length; i++){ 
+            taskList[i].innerHTML = arr[i];
         }
         li.append(arr[arr.length - 1])
-        node.appendChild(li);
-        h3.innerHTML = elements.length+" TASKS";
-        
-        msg = "새로운 할일 '"+newItem+"' 을 추가했습니다.";
-        showMessage(msg);
-    } 
+        taskNode.appendChild(li);
+        msg = SUCCESS_MSG.TODO.ADD_TODO;
+    }
+    return msg;
 }
 
-// -- 목록 삭제 -- //
-function removeItem(num) {
-    const node = document.getElementsByTagName("ol")[0];
-    var h3 = document.getElementsByTagName("h3")[0];
-    var elements = node.children;
+
+// -- DELETE TASK IN LIST -- //
+function removeTask(num) {
     var msg;
-    // 입력 값이 string일 경우
-    if (num.match(/^[0-9]+$/) === null){
-        msg = "숫자를 입력하세요.";
-        showMessage(msg);
+    if (taskList.length <= 0){
+        msg = ERROR_MSG.TODO.EMPTY;
     }
-    // 입력 값이 숫자일 경우 : typeof(num) = string
     else {
-        // 목록 숫자보다 큰 숫자를 입력할 경우, 번호 없음 메시지 호출
-        if(elements.length >= 1 && num >= elements.length+1) {
-            msg = "목록에 없는 번호입니다.";
-            showMessage(msg);
+        try { 
+            taskNode.removeChild(taskList[num-1])
+            msg = SUCCESS_MSG.TODO.REMOVE_TODO;
         }
-
-        // 목록이 없는 경우, 목록 없음 메시지 호출
-        else if (elements.length <= 0){
-            msg = "현재 할일이 없습니다. 새로운 할일을 추가하세요.";
-            h3.innerHTML = " 0 TASK";
-            showMessage(msg);
-        }
-        // 0을 입력할 경우
-        else if (num == 0) {
-            msg = "1부터 입력하세요";
-            showMessage(msg);
-        } 
-        // 목록 숫자보다 작은 숫자를 입력할 경우 아이템 삭제
-        else {  
-            node.removeChild(elements[num-1])
-            // 마지막 아이템 삭제 시, 목록 없음 메시지 호출
-            if (elements.length <= 0){
-                h3.innerHTML = " 0 TASK";
-                msg = "현재 할일이 없습니다. 새로운 할일을 추가하세요.";
-                showMessage(msg);
-            }
-            // 할일 삭제 시, 메세지 호출
-            else {
-                msg = " ' "+elements[num-1].textContent+"'할일이 삭제되었습니다."; 
-                h3.innerHTML = elements.length+" TASKS";
-                showMessage(msg);
-            }         
+        catch(err){
+            return chkErrInput(num);         
         }
     }
+    return msg;
 }
 
-// -- 메시지 호출 -- //
+    
+    
+// -- TASK NUMBERING -- //
+function changeTasksNum(){
+    countTask.innerHTML = taskList.length+" TASKS";
+}
+
+
+// -- CALL MESSAGES -- //
 function showMessage(text){
-    node = document.getElementsByClassName("message")[0];
-    elements = node.children;
+    tasknode = document.getElementsByClassName("message")[0];
     p = document.createElement("p");
     p.innerHTML = text;
-    node.appendChild(p);
+    tasknode.appendChild(p);
     setTimeout(function(){ p.style.display = "none"; }, 2000);
 }
 
-// -- 버튼 이벤트 -- //
+
+// -- BUTTON EVENT -- //
 var controller = document.querySelector(".controller");
 controller.addEventListener("click", function(evt) {
   var btn = evt.target;
@@ -101,18 +119,35 @@ controller.addEventListener("click", function(evt) {
   executeItemNode(actionType, inputValue);
 });
 
+
+// -- EXECUTE -- //
 function executeItemNode(actionType, todoORNumber){
-    var msg;
-    if (todoORNumber.length >= 1) {
-        if (actionType === "add") {
-            addItem(todoORNumber);
-        }
-        if (actionType === "remove") {
-            removeItem(todoORNumber);
-        }
+    if (actionType === "add") {
+        var a = addTask(todoORNumber);
+        showMessage(a);
     }
-    else {
-        msg = "입력하세요."
-        showMessage(msg);
+    else if (actionType === "remove") {
+        var r = removeTask(todoORNumber);
+        showMessage(r);
     }
+    changeTasksNum();
 }
+
+
+// // 포커스
+// var controller = document.querySelector(".controller");
+// controller.addEventListener("click", function(evt) {
+//   var btn = evt.target;
+//   if (btn.tagName !== "BUTTON") return;
+//   var inputValue = btn.previousElementSibling.value;
+//   var actionType = btn.className;
+//   executeItemNode(actionType, inputValue);
+// });
+
+
+
+// function setCSS(ele, styleObj) {
+//     ele.style[styleName] = value;
+//for in
+//}
+// setCSS(ele, {"opacity": 0.5, "display":"none"})
