b5372c823883122f439b8b08e930e73fdc0d033a
Jeewhan <distinct19@gmail.com>
2017-01-28 20:37:25 +0900

    Update

--- Jeewhan/infinitySlider/script.js
+++ Jeewhan/infinitySlider/script.js
@@ -1,7 +1,3 @@
-// TODO: slide를 absoulute + left로 조정하는 것이 아니라, relative로 조정
-// TODO: indicator 클릭시 무조건 오른쪽으로 흐르도록 수정
-// (5->1->2->3->4, 현재 화면 1번에서 5번 indicator를 누르면 오른쪽으로 가면서 2,3,4를 스치고 5번을 만남)
-
 const slideBox = document.querySelector(".slideBox");
 const indicator = document.querySelector(".indicator");
 const directionBox = document.querySelector(".directionBox");
@@ -13,12 +9,13 @@ function getLeftValue(element) {
   return window.getComputedStyle(element).getPropertyValue("left").slice(0, -2) * -1;
 }
 
-function moveSlide(direction, xValue) {
+// 무한스크롤을 위해 반대방향 끝에 있는 슬라이드를 이동
+function moveEdgeSlide(direction, xValue) {
   if (direction === "left") {
     const clone = slideBox.lastElementChild.cloneNode();
 
     clone.innerHTML = slideBox.lastElementChild.innerHTML;
-    clone.style.left = xValue + bifurcation[direction] * -1 + "px";
+    clone.style.left = (xValue + bifurcation[direction]) * -1 + "px";
 
     slideBox.insertBefore(clone, slideBox.firstElementChild);
     slideBox.lastElementChild.remove();
@@ -34,6 +31,7 @@ function moveSlide(direction, xValue) {
   }
 }
 
+// 화살표에 따라 슬라이드 보여주는 박스를 이동
 function controlBox(direction) {
   const xValue = slideBox.style.transform.replace(/translate3d\((.?\d+)px.+/, "$1") * 1;
   const firstSlideLeft = getLeftValue(slideBox.firstElementChild);
@@ -41,7 +39,7 @@ function controlBox(direction) {
 
   if ((direction === "left" && xValue === firstSlideLeft) ||
      (direction === "right" && xValue === lastSlideLeft)) {
-    moveSlide(direction, xValue);
+    moveEdgeSlide(direction, xValue);
   }
   slideBox.style.transform = "translate3d(" + (xValue + bifurcation[direction]) + "px, 0, 0)";
 }
@@ -60,3 +58,7 @@ indicator.addEventListener("click", (evt) => {
 
   slideBox.style.transform = "translate3d(" + getLeftValue(location[0]) + "px, 0, 0)";
 });
+
+// TODO: slide를 absoulute + left가 아닌 relative + float left로 바꿀 수 있는가?
+// TODO: indicator 클릭시 무조건 오른쪽으로 흐르도록 수정
+// (5->1->2->3->4, 현재 화면 1번에서 5번 indicator를 누르면 오른쪽으로 가면서 2,3,4를 스치고 5번을 만남)
