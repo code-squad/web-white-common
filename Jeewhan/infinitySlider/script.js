8163041c8d8bd26878dd154211b0b3c96881d9b2
Jeewhan <distinct19@gmail.com>
2017-01-28 15:19:14 +0900

    Update

--- Jeewhan/infinitySlider/script.js
+++ Jeewhan/infinitySlider/script.js
@@ -1,10 +1,13 @@
+// TODO: slide를 absoulute + left로 조정하는 것이 아니라, relative로 조정
+// TODO: indicator 클릭시 무조건 오른쪽으로 흐르도록 수정
+// (5->1->2->3->4, 현재 화면 1번에서 5번 indicator를 누르면 오른쪽으로 가면서 2,3,4를 스치고 5번을 만남)
+
 var slideBox = document.querySelector(".slideBox");
 var indicator = document.querySelector(".indicator");
 var directionBox = document.querySelector(".directionBox");
 const slideWidth = window.getComputedStyle(slideBox.firstElementChild).getPropertyValue("width").slice(0,-2) * 1;
 
 const bifurcation = { left: slideWidth, right: -slideWidth };
-
 
 function getLeftValue(node) {
   return window.getComputedStyle(node).getPropertyValue("left").slice(0,-2) * -1;
@@ -51,14 +54,13 @@ directionBox.addEventListener("click",
 
 
 indicator.addEventListener("click", (evt) => {
-  let slideAll = document.querySelectorAll("div.slide");
   let iconIndex = evt.target.className.replace(/circle(\d+)$/, "$1") * 1;
+  let slideAll = document.querySelectorAll("div.slide");
   let location = 0;
 
   for (let slide of slideAll) {
     if (iconIndex === slide.className.slice(-1) * 1) {
-      location = window.getComputedStyle(slide).getPropertyValue("left").slice(0,-2) * -1;
-      break;
+      location = getLeftValue(slide); break;
     }
   }
   slideBox.style.transform = "translate3d(" + location + "px, 0, 0)";
