50706d8df0680850a14c8723c61962aa5954e95c
Jeewhan <distinct19@gmail.com>
2017-01-30 11:59:17 +0900

    Update

--- Jeewhan/infinitySlider/script.js
+++ Jeewhan/infinitySlider/script.js
@@ -15,16 +15,15 @@ function moveEdgeSlide(direction, xValue) {
     const clone = slideBox.lastElementChild.cloneNode();
 
     clone.innerHTML = slideBox.lastElementChild.innerHTML;
-    clone.style.left = (xValue + bifurcation[direction]) * -1 + "px";
+    clone.style.left = `${(xValue + bifurcation[direction]) * -1}px`;
 
     slideBox.insertBefore(clone, slideBox.firstElementChild);
     slideBox.lastElementChild.remove();
-  }
-  else if (direction === "right") {
+  } else {
     const clone = slideBox.firstElementChild.cloneNode();
 
     clone.innerHTML = slideBox.firstElementChild.innerHTML;
-    clone.style.left = (xValue + bifurcation[direction]) * -1 + "px";
+    clone.style.left = `${(xValue + bifurcation[direction]) * -1}px`;
 
     slideBox.appendChild(clone);
     slideBox.firstElementChild.remove();
@@ -32,7 +31,7 @@ function moveEdgeSlide(direction, xValue) {
 }
 
 // 화살표에 따라 슬라이드 보여주는 박스를 이동
-function controlBox(direction) {
+function moveBox(direction) {
   const xValue = slideBox.style.transform.replace(/translate3d\((.?\d+)px.+/, "$1") * 1;
   const firstSlideLeft = getLeftValue(slideBox.firstElementChild);
   const lastSlideLeft = getLeftValue(slideBox.lastElementChild);
@@ -41,25 +40,29 @@ function controlBox(direction) {
      (direction === "right" && xValue === lastSlideLeft)) {
     moveEdgeSlide(direction, xValue);
   }
-  slideBox.style.transform = "translate3d(" + (xValue + bifurcation[direction]) + "px, 0, 0)";
+  slideBox.style.transform = `translate3d(${xValue + bifurcation[direction]}px, 0, 0)`;
 }
 
-directionBox.addEventListener("click",
-  (evt) => { controlBox(evt.target.className); }
-);
+// DOM이 다 불려진 뒤에, 화살표와 인디케이터에 이벤트 등록
+document.addEventListener("DOMContentLoaded", () => {
+  directionBox.addEventListener("click",
+    (evt) => { moveBox(evt.target.className); }
+  );
 
-indicator.addEventListener("click", (evt) => {
-  const iconIndex = evt.target.className.replace(/circle(\d+)$/, "$1") * 1;
-  const slideAll = document.querySelectorAll("div.slide");
+  indicator.addEventListener("click", (evt) => {
+    const iconIndex = evt.target.className.replace(/circle(\d+)$/, "$1") * 1;
+    const slideAll = document.querySelectorAll("div.slide");
 
-  // http://www.bsidesoft.com/?p=323
-  const location = Array.prototype.slice.call(slideAll).filter((slide) => {
-    if (iconIndex === slide.className.slice(-1) * 1) { return true; }
+    // http://www.bsidesoft.com/?p=323
+    const location = Array.prototype.slice.call(slideAll).filter((slide) => {
+      if (iconIndex === slide.className.match(/\d+/)[0] * 1) { return true; }
+      return false;
+    });
+
+    slideBox.style.transform = `translate3d(${getLeftValue(location[0])}px, 0, 0)`;
   });
-
-  slideBox.style.transform = "translate3d(" + getLeftValue(location[0]) + "px, 0, 0)";
 });
 
 // TODO: slide를 absoulute + left가 아닌 relative + float left로 바꿀 수 있는가?
 // TODO: indicator 클릭시 무조건 오른쪽으로 흐르도록 수정
-// (5->1->2->3->4, 현재 화면 1번에서 5번 indicator를 누르면 오른쪽으로 가면서 2,3,4를 스치고 5번을 만남)
+// (5->1->2->3->4, 현재 화면 1번에서 5번 indicator를 누르면 오른쪽으로 가면서 2,3,4를 스치고 5번을 만나도록)
