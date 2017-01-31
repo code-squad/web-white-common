b973674fec6d232a08e257b1e1775ff65cd70409
sujinleeme <sujinlee.me@gmail.com>
2017-01-31 10:59:57 +0900

    finish silder homework

--- sujinlee/20170125-slider/app/js/script.js
+++ sujinlee/20170125-slider/app/js/script.js
@@ -1,63 +1,81 @@
-section = Array.prototype.slice.call(document.getElementsByTagName('section'));
+sections = Array.prototype.slice.call(document.getElementsByTagName('section'));
 buttons = document.getElementsByClassName('btn-container')[0];
 mainView = document.getElementsByClassName('mainView')[0];
 
-var width = 600;
+var width = 100;
 var i = 0;
-
-
+var slides = sections.length;
 
 buttons.addEventListener("click", function(evt){
     var target = evt.target;
-    
+    if (target.tagName === "SPAN") {
+        target = target.parentNode;
+    }
     if (target.className === "next"){
-        console.log(i);
-        if (i>3){i = i;}
+        if (i > slides-1) {i = slides % i;}
+        if (i === slides) {i = 0;}
         else { i++; }
         moveNext(i);
     }
-   
-    if (target.className === "previous"){
+    if (target.className === "previous"){     
+       if (i < 1) {i = slides - i;}
         movePrevious(i);
-        i--;
+        i--; 
     }
-    //moveNext(i);
 });
-function moveNext(i){
 
-    if (i < section.length) {
-        mainView.style.transform = "translate(-"+width*i+"px, 0px)";
+function moveNext(i){
+    if (i > slides-1) {
+        mainView.style.transform = "translate(0vw, 0vh)";
+        mainView.classList.add("fadeIn");
+         setTimeout(function() {
+            mainView.classList.remove("fadeIn");
+        }, (1000)); 
+        
     }
-    else { return; }
+    else {
+        mainView.classList.remove("fadeIn");
+        mainView.style.transform = "translate(-"+width*i+"vw, 0vh)";
+    }
 }
 
 function movePrevious(i){
-    if ( 0 < i && i < section.length) {
-        mainView.style.transform = "translate("+(width-(width*i))+"px, 0px)";
+    mainView.style.transform = "translate("+(width-(width*i))+"vw, 0vh)";
+    if (i > slides-1) {
+        mainView.classList.add("fadeIn");
+        setTimeout(function() {
+            mainView.classList.remove("fadeIn");
+        }, (1000)); 
     }
-
-    else { return; }
+    else {
+        mainView.classList.remove("fadeIn");
+    }
 }
 
+function fecthData(url) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+      try {
+          var data = JSON.parse(this.responseText);
+          console.log("Get Data Successfully.");
+          parseData(data);
+      } catch(err){
+          console.log("Fail to get data.")
+      }
+    }
+  };
+  xhr.open("GET", url, true);
+  xhr.send();
+}
 
-// var width = 600;
-// var count = 0;
-// previousBtn.addEventListener("click", function(evt){
-//     var target = evt.target;
-//     count ++ ;
- 
-//     if (count < section.length) {
-//         //console.log(width*count);
-//         console.log(count);
-//         console.log(width-(width*count));
-//         mainView.style.transform = "translate("+(width-(width*count))+"px, 0px)";
-//         mainView.style.animation = "moveSilde";
-//         // mainView.removeClass("animation");
-//         //console.log(mainView.style.transform);
-//     }
-//     else { return; }
-//     }
-// );
+function parseData(data){    
+    for (var e=0; e < slides; e++){
+        var randomNum = Math.floor(Math.random() * data.length);
+        sections[e].insertAdjacentHTML('beforeend', '<p>'+data[randomNum]+'</p>');
+    }
+}
 
-
+var quoteUrl = 'http://proverbs-app.antjan.us/'
+fecthData(quoteUrl);
 
